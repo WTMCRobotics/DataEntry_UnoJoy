7e537b126ad1f57731fd2086aa163d1a11827a21
Caroline Ham <cyham@wccnet.edu>
2018-01-10 16:20:09 -0500

    Working Version (debounce2 function)

--- DataEntry_UnoJoy.ino
+++ DataEntry_UnoJoy.ino
@@ -13,25 +13,26 @@ byte  btRawBTNS;          // actual state of buttons
 byte  btDebounceTimer[8]; // debounce timer for buttons
 byte  btBTNS;             // debounced state of buttons
 
+int rawReads[8];
+int newState[8];
+
 //**********************************************************************
 //  Initialize Hardware
 //
 void setup() 
 {
-  for (int i=0; i<8; i++)
-    pinMode(pinButtons[i], INPUT_PULLUP);
+  for (int i=2; i<12; i++)
+  {
+    pinMode(i, INPUT);
+    digitalWrite(i, HIGH);
+  }
 
-  for (int i=0; i<3; i++)
-    pinMode(pinID[i], INPUT_PULLUP);
-
-  btID = digitalRead(pinID[0]) << 0 |
-         digitalRead(pinID[1]) << 1 |
-         digitalRead(pinID[2]) << 2;
+//  btID = digitalRead(pinID[0]) << 0 |
+//         digitalRead(pinID[1]) << 1 |
+//         digitalRead(pinID[2]) << 2;
         
-    
-  Serial.begin(115200);
-  Serial.println("Hello World");
-//  setupUnoJoy();
+  setupUnoJoy();
+   Serial.begin(38400);
 }
 
 //**********************************************************************
@@ -40,15 +41,10 @@ void setup()
 void loop() 
 {
   ScanIO();
-  DebounceButtons();
+  debounce2();
+  //DebounceButtons();
 //  Serial.print(btRawBTNS, HEX);
-  Serial.print("0x");
-  if (btBTNS < 0x10)
-    Serial.print("0");
-  Serial.print(btBTNS, HEX);
   UpdateUnoJoy();
-
- // delay(500);
 }
 
 
@@ -58,11 +54,23 @@ void loop()
 //
 void ScanIO(void)
 {
+  /*
     btRawBTNS = 0;
     for (int i=0; i<8; i++)
     {
       btRawBTNS |= !digitalRead(pinButtons[i]) << i;
     }
+    */
+
+    rawReads[0] = !digitalRead(2);
+    rawReads[1] = !digitalRead(3);
+    rawReads[2] = !digitalRead(4);
+    rawReads[3] = !digitalRead(5);
+    rawReads[4] = !digitalRead(6);
+    rawReads[5] = !digitalRead(7);
+    rawReads[6] = !digitalRead(8);
+    rawReads[7] = !digitalRead(9);
+    
 }
 
 //********************************************************************
@@ -135,6 +143,60 @@ void DebounceButtons(void)
   lastBTNS = btBTNS;
 }
 
+void debounce2(void)
+{
+  int previous[8];
+  static uint32_t lastTime;
+  
+  uint32_t currentTime = millis();
+  uint32_t deltaTime = currentTime - lastTime;
+  
+  // return if delta < 1 mSec
+  if (deltaTime == 0)
+    return;
+
+    for (int i=0; i<8; i++)
+  {
+
+    // check next bit's debounceTimer
+    if (btDebounceTimer[i] > deltaTime)
+    {
+      // still active, update and continue
+      btDebounceTimer[i] -= deltaTime;
+//      Serial.print(" debouncing ");
+//      Serial.println(btDebounceTimer[i]);
+      continue;
+    }
+    else if (btDebounceTimer[i] > 0)
+    {
+      // debounce timed out
+      btDebounceTimer[i] = 0;
+//      Serial.print(" zero timer");
+      if (previous[i] != rawReads[i])
+      {
+        if (rawReads[i] == 0)
+        {
+          newState[i] = 0;
+        }
+        else
+        {
+          newState[i] = 1;
+        } 
+      }
+    }
+    else
+    {
+      // start debounc timer
+      if (previous[i] != rawReads[i])
+        btDebounceTimer[i] = DEBOUNCE_TIME;
+    }  
+  }
+  for(int i = 0; i < 8; i++)
+  {
+    previous[i] = newState[i];
+  }
+  
+}
 //**********************************************************************
 //  Process button presses
 //
@@ -142,14 +204,13 @@ void UpdateUnoJoy()
 {
     // Always be getting fresh data
   dataForController_t controllerData = getControllerData();
-//  setControllerData(controllerData);
+  setControllerData(controllerData);
 //  Serial.println(btBTNS, HEX);
-  printControllerData(controllerData);
+ // printControllerData(controllerData);
 }  
 
 dataForController_t getControllerData(void)
 {
-  
   // Set up a place for our controller data
   //  Use the getBlankDataForController() function, since
   //  just declaring a fresh dataForController_t tends
@@ -159,56 +220,55 @@ dataForController_t getControllerData(void)
   // Since our buttons are all held high and
   //  pulled low when pressed, we use the "!"
   //  operator to invert the readings from the pins
-  controllerData.triangleOn   = (btBTNS & 0x01) ? 1 : 0;
-  controllerData.circleOn     = (btBTNS & 0x02) ? 1 : 0;
-  controllerData.squareOn     = (btBTNS & 0x04) ? 1 : 0;
-  controllerData.crossOn      = (btBTNS & 0x08) ? 1 : 0;
-  controllerData.dpadUpOn     = (btBTNS & 0x10) ? 1 : 0;
-  controllerData.dpadDownOn   = (btBTNS & 0x20) ? 1 : 0;
-  controllerData.dpadLeftOn   = (btBTNS & 0x40) ? 1 : 0;
-  controllerData.dpadRightOn  = (btBTNS & 0x80) ? 1 : 0;
-  
-  controllerData.l1On         = !digitalRead(pinID[0]);
-  controllerData.r1On         = !digitalRead(pinID[1]);
-  controllerData.selectOn     = !digitalRead(pinID[2]);
-  
-  //controllerData.startOn = !digitalRead(A4);
-  //controllerData.homeOn = !digitalRead(A5);
+  controllerData.triangleOn   = newState[0];
+  controllerData.circleOn     = newState[1];
+  controllerData.squareOn     = newState[2];
+  controllerData.crossOn      = newState[3];
+  controllerData.dpadUpOn     = newState[4];
+  controllerData.dpadDownOn   = newState[5];
+  controllerData.dpadLeftOn   = newState[6];
+  controllerData.dpadRightOn  = newState[7];
+
+  controllerData.l1On = 0;
+  controllerData.r1On = 0;
+  controllerData.selectOn = 0;
+  controllerData.startOn = 0;
+  controllerData.homeOn = 0;
   
   // Set the analog sticks
   //  Since analogRead(pin) returns a 10 bit value,
   //  we need to perform a bit shift operation to 
   //  lose the 2 least significant bits and get an
   //  8 bit number that we can use  
-  controllerData.leftStickX  = analogRead(A0) >> 2;
-  controllerData.leftStickY  = analogRead(A1) >> 2;
-  controllerData.rightStickX = analogRead(A2) >> 2;
-  controllerData.rightStickY = analogRead(A3) >> 2;
+  controllerData.leftStickX  = 128;
+  controllerData.leftStickY  = 128;
+  controllerData.rightStickX = 128;
+  controllerData.rightStickY = 128;
   // And return the data!
   return controllerData;
 }
 
-void printControllerData(dataForController_t controllerData)
-{
-  byte btID = (controllerData.l1On << 0) +
-              (controllerData.r1On << 1) +
-              (controllerData.selectOn << 2);
-  Serial.print(" ID:");
-  Serial.print(btID, HEX);
-  Serial.print(" T:");
-  Serial.print(controllerData.triangleOn);
-  Serial.print(" C-");
-  Serial.print(controllerData.circleOn);
-  Serial.print(" S-");
-  Serial.print(controllerData.squareOn);
-  Serial.print(" C-");
-  Serial.print(controllerData.crossOn);
-  Serial.print(" U-");
-  Serial.print(controllerData.dpadUpOn);
-  Serial.print(" D-");
-  Serial.print(controllerData.dpadDownOn);
-  Serial.print(" L-");
-  Serial.print(controllerData.dpadLeftOn);
-  Serial.print(" R-");
-  Serial.println(controllerData.dpadRightOn);
- }  
+//void printControllerData(dataForController_t controllerData)
+//{
+//  byte btID = (controllerData.l1On << 0) +
+//              (controllerData.r1On << 1) +
+//              (controllerData.selectOn << 2);
+//  Serial.print(" ID:");
+//  Serial.print(btID, HEX);
+//  Serial.print(" T:");
+//  Serial.print(controllerData.triangleOn);
+//  Serial.print(" C-");
+//  Serial.print(controllerData.circleOn);
+//  Serial.print(" S-");
+//  Serial.print(controllerData.squareOn);
+//  Serial.print(" C-");
+//  Serial.print(controllerData.crossOn);
+//  Serial.print(" U-");
+//  Serial.print(controllerData.dpadUpOn);
+//  Serial.print(" D-");
+//  Serial.print(controllerData.dpadDownOn);
+//  Serial.print(" L-");
+//  Serial.print(controllerData.dpadLeftOn);
+//  Serial.print(" R-");
+//  Serial.println(controllerData.dpadRightOn);
+// }  
